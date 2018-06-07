f9b16b0e35d58ee42dbe61520f12f1c8caebf620
Hans <635260101@qq.com>
2018-06-07 17:30:37 +0800

    test

--- AccessApiOne/Program.cs
+++ AccessApiOne/Program.cs
@@ -10,7 +10,7 @@ namespace AccessApiOne
     {
         static void Main(string[] args)
         {
-            Test().Wait();
+            Test().Wait(); 
             Console.ReadKey();
         }
 
